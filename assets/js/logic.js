da1865f2e378d37a93b4df16b854780b7cf4f345
JT <jstorbit@gmail.com>
2025-02-02 16:44:13 -0700

    added sencond logic for window sizing

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,24 @@
 function newWindow() {
     var newUrl = prompt("What web address would you like to visit?", newUrl);
     var newWin = window;
-    var windowOptions = "menubar,location,scrollbars,stats"
+    var windowOptions = "menubar,location,scrollbars,stats";
     newWin.open("http://" + newUrl, "New URL Window", windowOptions);
     return
 }   
+
+function winSize() {
+    var newWin = window;
+    var windowOptions = "menubar=no,location,scrollbars,stats=yes, top=200, left=200, width=600, height=400";
+    var isHttp = false;
+    while (!isHttp) {
+        var newUrl = prompt("What web address would you like to visit? ex: http://www.example.com", newUrl);
+        var toBegin = newUrl.startsWith('http://');
+        if (!toBegin) {
+            alert("Must start with http://");
+        }
+        else {
+            isHttp = true;
+        }
+    }
+    newWin.open(newUrl, "New URL Window", windowOptions);
+}
